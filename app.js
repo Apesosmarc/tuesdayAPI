9a1eb0313702fbc1fa562815caff57616d4e7a54
Marc Apesos <apesos2013@gmail.com>
2022-03-08 12:24:42 -0500

    herokuy setup

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("tuesday api");
+});
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
